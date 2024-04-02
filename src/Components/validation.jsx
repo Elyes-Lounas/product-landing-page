409fde830d740f10635c6cb038d5305c008bb64d
Elyes-Lounas <121710591+Elyes-Lounas@users.noreply.github.com>
2024-04-02 05:38:26 +0100

    Update validation.jsx

--- src/Components/validation.jsx
+++ src/Components/validation.jsx
@@ -1,7 +1,7 @@
 export default function Validation(wilaya, quantity, priceOfDelivery) {
   let errors = {};
-  if (!wilaya) errors.wilaya = "wilaya is required!";
-  if (!quantity) errors.quantity = "quantity is required!";
-  if (!priceOfDelivery) errors.priceOfDelivery = "price of delivery is required!";
+  if (!wilaya) errors.wilaya = "يرجى إختيار الولاية";
+  if (!quantity) errors.quantity = "يرجى إخيار الكمية";
+  if (!priceOfDelivery) errors.priceOfDelivery = "يرجى إختيار مكان التوصيل";
   return errors;
 }
